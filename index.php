d6c47d013c43fe62d4c8d8642c50789a93746279
Roldan Barbante <rbarbante@mail.greenriverdev.com>
2020-05-01 17:02:34 -0400

    process order, didn't finish before end of class

--- index.php
+++ index.php
@@ -11,14 +11,27 @@ $f3 = Base::instance();
 
 //define a default root (what the user sees when they go to index page)
 $f3->route('GET /', function() {
-    $view = new Template();
-    echo $view->render('views/pet-home.html');
+    echo "<h1>My Pets</h1>";
+    echo "<a href='order'>Order a Pet</a>";
+    /*$view = new Template();
+    echo $view->render('views/home.html');*/
+
 });
 
-$f3->route('GET /order', function() {
+$f3-> route('GET|POST /order', function() {
+    //check if the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        ECHO "POST METHOD";
+    }
+    else {
+        echo "";
+    }
+
     $view = new Template();
-    echo $view->render('views/order.html');
-});
+    echo $view->render("vie")
+}
+
+
 
 //run fat free
 $f3->run();
